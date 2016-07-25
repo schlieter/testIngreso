3d63da2bed6cc1cb5f125f773ad000abdc3bc222
schlieter <c.schlieter@hotmail.com>
2016-07-25 16:59:32 -0300

    while

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,14 +1,27 @@
 function Mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
+	var maximo = 0;
+	var minimo = 0;
 	var respuesta='si';
-
+	
 	while(respuesta!='no')
 	{
-		
+		var numero = prompt("Ingrese numero:");
+		if(numero < minimo)
+		{
+			minimo = parseInt(numero)+ 0;
+		}
+		else
+		{
+			if(numero > maximo)
+			{
+				maximo = parseInt(numero) + 0;
+			}
+		}
+		document.getElementById('minimo').value = minimo;
+		document.getElementById('maximo').value = maximo;
+		respuesta = prompt("Quiere seguir ingresando numeros?");
 	
 	}
 
