7e39916ae60275fd626eae302f2d30e13280037d
schlieter <c.schlieter@hotmail.com>
2016-08-02 21:57:39 -0300

    2-8

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,69 @@
 function Mostrar()
 {
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
-
-	while(respuesta!="no")
+	var numero;
+	var respuesta = "si";
+	var contadorP = 0;
+	var contadorN = 0;
+	var contadorCero= 0;
+	var contadorPar = 0;
+	var acumuladorN = 0;
+	var acumuladorP = 0;
+	while(1)
 	{
-		
-	
+		if(respuesta == "si")
+		{
+			numero = prompt("Ingrese numero");
+			numero = parseInt(numero);
+			while(isNaN(numero))
+			{
+				numero = prompt("Ingrese numero");
+				numero = parseInt(numero);
+			}
+			if(numero < 0)
+			{
+				contadorN++;
+				acumuladorN = acumuladorN + numero;
+				if(numero%2 == 0)
+				{
+					contadorPar++;
+				}
+			}
+			else
+			{
+				if(numero > 0)
+				{
+					contadorP++;
+					acumuladorP = acumuladorP + numero;
+					if(numero%2== 0)
+					{
+						contadorPar++;
+					}
+				}
+				else
+				{
+					contadorCero++;
+				}
+			}
+		}
+		else
+		{
+			break;
+		}
+		respuesta = prompt("Quiere seguir ingresando numeros?");
 	}
+	alert("La suma de los negativos es: "+acumuladorN);
+	alert("La suma de los positivos es: "+acumuladorP);
+	alert("La cantidad de numeros positivos es: "+contadorP);
+	alert("La cantidad de numeros negativos es: "+contadorN);
+	alert("La cantidad de ceros es: "+contadorCero);
+	alert("La cantidad de numeros pares es: "+contadorPar);
+	alert("El promedio de los positivos es: "+(acumuladorP/contadorP));
+	alert("El promedio de los negativos es: "+(acumuladorN/contadorN));
+	alert("La diferencia entre los positivos y negativos es: "+(acumuladorP+acumuladorN));
 
 
 
 
-}//FIN DE LA FUNCIÓN
+
+}
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,29 +1,47 @@
 function Mostrar()
 {
 
-	var maximo = -10000;
-	var minimo = 10000;
+	var maximo;
+	var minimo; 
+	var contador = 0;
 	var respuesta='si';
+	var numero;
 	
-	while(respuesta!='no')
+	while(1)
 	{
-		var numero = prompt("Ingrese numero:");
-		if(numero < minimo)
+		if(respuesta == "si")
 		{
-			minimo = parseInt(numero)+ 0;
-		}
+			contador++;
+			var numero = prompt("Ingrese numero:");
+			numero = parseInt(numero);
+			if(contador == 1)
+			{
+				minimo = numero;
+				maximo = numero;
+			}
+			else
+			{
+				if(numero < minimo)
+				{
+					minimo = numero;
+				}
+				if(numero > maximo)
+				{
+					maximo= numero;
+				}
+			}
+		}	
 		else
 		{
-			if(numero > maximo)
+			if(respuesta == "no")
 			{
-				maximo = parseInt(numero) + 0;
+				break;
 			}
 		}
-		document.getElementById('minimo').value = minimo;
-		document.getElementById('maximo').value = maximo;
 		respuesta = prompt("Quiere seguir ingresando numeros?");
-	
 	}
+	document.getElementById('minimo').value = minimo;
+	document.getElementById('maximo').value = maximo;
 
 
 
